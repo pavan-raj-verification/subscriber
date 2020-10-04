30337adb21fd578b6bf3f4c8de84c30488c0ddc3
R PAVAN RAJ <69004613+stepin104304@users.noreply.github.com>
2020-10-04 19:09:38 +0530

    Add files via upload

---
+++ inc/subscriber.h
@@ -0,0 +1,177 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<string.h>
+struct subscriber
+{
+char accountNumber[20];
+char phonenumber[20];
+char name[50];
+float amount;
+}s;
+void addrecords();
+void listrecords();
+void modifyrecords();
+void searchrecords();
+void payment();
+char get;
+void addrecords()
+{
+	FILE *f;
+	char test;
+	f=fopen("c:/file10.ojs","ab+");
+	if(f==0)
+	{   f=fopen("c:/file10.ojs","wb+");
+		system("cls");
+		printf("please wait while we configure your computer");
+		printf("/npress any key to continue");
+		getch();
+	}
+	while(1)
+	{
+		system("cls");
+		printf("\n Enter phone number:");
+		scanf("%s",&s.phonenumber);
+		printf("\n Enter name:");
+		fflush(stdin);
+		scanf("%[^\n]",&s.name);
+		printf("\n Enter Account number:");
+		fflush(stdin);
+		scanf("%[^\n]",&s.accountNumber);
+		printf("\n Enter amount:");
+		scanf("%f",&s.amount);
+		fwrite(&s,sizeof(s),1,f);
+		fflush(stdin);
+		system("cls");
+		printf("1 record successfully added");
+		printf("\n Press esc key to exit, any other key to add other record:");
+		test=getche();
+		if(test==27)
+			break;
+	}
+	fclose(f);
+}
+void listrecords()
+{
+	FILE *f;
+	int i;
+	if((f=fopen("c:/file10.ojs","rb"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Phone Number\t\tUser Name\t\tAccount Number\t\t\tAmount\n");
+	for(i=0;i<89;i++)
+		printf("-");
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		printf("\n%-10s\t\t%-10s\t\t%-10s\t\tRs. %.2f /-",s.phonenumber,s.name,s.accountNumber,s.amount);
+	}
+	printf("\n");
+	for(i=0;i<89;i++)
+		printf("-");
+
+fclose(f);
+getch();
+}
+void searchrecords()
+{
+	FILE *f;
+	char phonenumber[20];
+	int flag=1;
+	f=fopen("c:/file10.ojs","rb+");
+	if(f==0)
+		exit(0);
+	fflush(stdin);
+	system("cls");
+	printf("Enter Phone Number to search in our database");
+	scanf("%s", phonenumber);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.phonenumber,phonenumber)==0)
+		{	system("cls");
+			printf(" Record Found ");
+			printf("\n\nPhonenumber: %s\nName: %s\nAccount Number: %s\nAmount: Rs.%0.2f\n",s.phonenumber,s.name,s.accountNumber,s.amount);
+			flag=0;
+			break;
+		}
+		else if(flag==1)
+		{	system("cls");
+			printf("Requested Phone Number Not found in our database");
+		}
+	}
+	getch();
+	fclose(f);
+}
+void modifyrecords()
+{
+	FILE *f;
+	char phonenumber[20];
+	long int size=sizeof(s);
+	if((f=fopen("c:/file10.ojs","rb+"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Enter phone number of the subscriber to modify:");
+	scanf("%[^\n]",phonenumber);
+	fflush(stdin);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.phonenumber,phonenumber)==0)
+		{
+			system("cls");
+			printf("\n Enter phone number:");
+			scanf("%s",&s.phonenumber);
+			printf("\n Enter name: ");
+			fflush(stdin);
+			scanf("%[^\n]",&s.name);
+			printf("\n Enter Account Number: ");
+			fflush(stdin);
+			scanf("%[^\n]",&s.accountNumber);
+			printf("\n Enter amount: ");
+			scanf("%f",&s.amount);
+			fseek(f,-size,SEEK_CUR);
+			fwrite(&s,sizeof(s),1,f);
+			break;
+		}
+	}
+	fclose(f);
+}
+void payment()
+{
+	FILE *f;
+	char phonenumber[20];
+	long int size=sizeof(s);
+	float amt;
+	int i;
+	if((f=fopen("c:/file10.ojs","rb+"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Enter phone number of the subscriber for payment");
+	scanf("%[^\n]",phonenumber);
+	fflush(stdin);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.phonenumber,phonenumber)==0)
+		{
+			system("cls");
+			printf("\n Phone No.: %s",s.phonenumber);
+			printf("\n Name: %s",s.name);
+			printf("\n Current amount: %f",s.amount);
+			printf("\n");
+			for(i=0;i<79;i++)
+				printf("-");
+			printf("\n\nEnter amount of payment :");
+			fflush(stdin);
+			scanf(" %f",&amt);
+			s.amount=s.amount-amt;
+			fseek(f,-size,SEEK_CUR);
+			fwrite(&s,sizeof(s),1,f);
+			break;
+		}
+	}
+	system("cls");
+	printf("THANK YOU %s FOR YOUR TIMELY PAYMENTS",s.name);
+	getch();
+	fclose(f);
+}
+
---
+++ src/subscriber.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+//added header file
+#include"subscriber.h"
+
+
+int main()
+{	int password;
+	int phonenumber;
+	char choice;
+
+
+
+	system("cls");
+
+	printf("\n\n\n\n\n\n\n\n\n***********************************************************");
+	printf("\n\t\t--------------WELCOME-------------");
+	printf("\n\t****************************************************************");
+	Sleep(2000);
+	getch();
+    system("cls");
+	while (1)
+	{
+		system("cls");
+		printf("\n enter\n 1 : for adding new records.\n 2 : for list of records");
+		printf("\n 3 : for modifying records.\n 4 : for payment");
+		printf("\n 5 : for searching records.");
+		choice=getche();
+		choice=toupper(choice);
+		switch(choice)
+		{
+			case '4':
+				payment();break;
+			case '1':
+				addrecords();break;
+			case '2':
+				listrecords();break;
+			case '3':
+				modifyrecords();break;
+			case '5':
+				searchrecords();break;
+			case '6':
+				system("cls");
+				printf("\n\n\t\t\t\tTHANK YOU");
+				printf("\n\n\n\n\n:\n\tFOR USING OUR SERVICE");
+				Sleep(2000);
+				exit(0);
+				break;
+			default:
+				system("cls");
+				printf("Incorrect Input");
+				printf("\nAny key to continue");
+				getch();
+		}
+	}
+}
